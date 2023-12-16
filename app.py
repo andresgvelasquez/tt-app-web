7b4b10d24af155fb4e5e98502aaf258bae9a09ba
andresgvelasquez <andresgvelasquez8@gmail.com>
2023-12-16 12:53:51 -0800

    update header

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import streamlit as st
 import plotly.express as px
 
 
-st.header('Conjunto de datos de anuncions de venta de coches') # Encabezado
+st.header('Conjunto de datos de anuncios de venta de coches') # Encabezado
 
 car_data = pd.read_csv('vehicles_us.csv') # Leer el dataframe vehicles_us
-hist_checkbox = st.checkbox('Construir histograma') # Crear un checkbox para el histograma
-scatter_checkbox = st.checkbox('Construir gráfico de dispersión') # Crear un checkbox para el gráfico de dispersión
+hist_checkbox = st.checkbox('Construir histograma.') # Crear un checkbox para el histograma
+scatter_checkbox = st.checkbox('Construir gráfico de dispersión.') # Crear un checkbox para el gráfico de dispersión
 
 if hist_checkbox: # Al hacer click en el checkbox del histograma
     # Escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches.')
 
     # Crea un histogrma
     fig = px.histogram(car_data, x='odometer')
@@ -21,7 +21,7 @@ if hist_checkbox: # Al hacer click en el checkbox del histograma
 
 if scatter_checkbox: # Al hacer click en el checkbox del gráfico de dispersión
     # Escribir un mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches.')
 
     # Crea un gráfico de dispersión
     fig = px.scatter(car_data, x='odometer', y='price')
