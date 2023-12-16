61a7b8a3811eae301d3c9e782169bba9a91b8ecd
andresgvelasquez <andresgvelasquez8@gmail.com>
2023-12-16 11:44:11 -0800

    add histogram and scatter checkbox to plot

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') # Leer el dataframe vehicles_us
+hist_checkbox = st.checkbox('Construir histograma') # Crear un checkbox para el histograma
+scatter_checkbox = st.checkbox('Construir gráfico de dispersión') # Crear un checkbox para el gráfico de dispersión
+
+
+if hist_checkbox: # Al hacer click en el checkbox del histograma
+    # Escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crea un histogrma
+    fig = px.histogram(car_data, x='odometer')
+
+    # Mostrar el histograma Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_checkbox: # Al hacer click en el checkbox del gráfico de dispersión
+    # Escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # Crea un gráfico de dispersión
+    fig = px.scatter(car_data, x='odometer', y='price')
+
+    # Mostrar el gráfico de dispersión Plotly intercativo
+    st.plotly_chart(fig, use_container_width=True)
+
